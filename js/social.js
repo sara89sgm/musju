e197647980ec36a13db000f420fa93fa1b880c5b
Sara Gozalo <sara89sgm@gmail.com>
2012-08-22 17:06:59 -0500

    scroll view

--- js/social.js
+++ js/social.js
@@ -20,7 +20,7 @@ $(function() {
 	function handleDragOver(e) {
 		e.preventDefault();
 		e.dataTransfer.dropEffect = 'copy'; // See the section on the
-											// DataTransfer object.
+		// DataTransfer object.
 		return false;
 	}
 
@@ -39,29 +39,25 @@ $(function() {
 			showTracks(uri);
 		}
 	}
-	
 
-	
 	if (navigator.geolocation) {
-		  navigator.geolocation.getCurrentPosition(successGeo, errorGeo);
-		} else {
-		  error('not supported');
-		}
-	
+		navigator.geolocation.getCurrentPosition(successGeo, errorGeo);
+	} else {
+		error('not supported');
+	}
 
 });
 
 function successGeo(position) {
-	  lng=position.coords.longitude;
-	  lat=position.coords.latitude;
-	  console.log("fine geolocation");
-	}
+	lng = position.coords.longitude;
+	lat = position.coords.latitude;
+	console.log("fine geolocation");
+}
 
-	function errorGeo(msg) {
-	
-	  
-	  console.log("error geolocation"+msg.code);
-	}
+function errorGeo(msg) {
+
+	console.log("error geolocation" + msg.code);
+}
 
 function showTracks(uriPl) {
 
@@ -84,7 +80,6 @@ function showTracks(uriPl) {
 function submitPlaylist() {
 	alert("Creating new Musju Playlist...");
 	checkPlaylistNoExists(newPlaylistURI);
-	
 
 }
 
@@ -98,8 +93,6 @@ function createNewPlaylist() {
 	newplaylist.set("latitude", 0.0);
 	newplaylist.set("longitude", 0.0);
 	newplaylist.set("idUser", sessionStorage.idUser)
-	
-	
 
 	newplaylist
 			.save(
@@ -109,7 +102,6 @@ function createNewPlaylist() {
 							alert("You have created a new Musju Playlist...start sharing it!!");
 							saveTracksPlaylist(newPlaylistURI);
 							showPlaylistImages(newPlaylistURI);
-							
 
 						},
 						error : function(newplaylist, error) {
@@ -118,78 +110,69 @@ function createNewPlaylist() {
 							// description.
 						}
 					});
-	
-	
+
 }
 
-function checkPlaylistNoExists(url){
+function checkPlaylistNoExists(url) {
 	var PlaylistMusju = Parse.Object.extend("PlaylistMusju");
 	var query = new Parse.Query(PlaylistMusju);
-	
+
 	query.equalTo("url", url);
-	query.find({
-	  success: function(results) {
-		  alert("playlists: "+results.length);
-		  if(results.length==0){
-			  createNewPlaylist();}else{
-				  alert("The playlist already exists, select it to listen to it and manage it" );
-			  }
-		 
-	   
-	  },
-	  error: function(error) {
-			
-			
-	    	  }
-	});
-	
+	query
+			.find({
+				success : function(results) {
+					alert("playlists: " + results.length);
+					if (results.length == 0) {
+						createNewPlaylist();
+					} else {
+						alert("The playlist already exists, select it to listen to it and manage it");
+					}
+
+				},
+				error : function(error) {
+
+				}
+			});
+
 }
-function saveTracksPlaylist(url){
+function saveTracksPlaylist(url) {
 
+	var tempPlaylist = new models.Playlist.fromURI(url);
 
-var tempPlaylist = new models.Playlist.fromURI(url);
-
-	
-	
 	tempPlaylist2 = new models.Playlist();
 	$.each(tempPlaylist.tracks, function(num, track) {
 
 		tempPlaylist2.add(models.Track.fromURI(track.uri));
 	});
-	
-	
+
 	// console.log(uriPlaylist.length);
 	var i = 0;
 	$.each(tempPlaylist2.tracks, function(num, track2) {
-           var TrackPlaylistMusju = Parse.Object.extend("TrackPlaylistMusju");
-	var newtrack = new TrackPlaylistMusju();
-	
-	
-	newtrack.set("urlTrack", track2.uri);
-	newtrack.set("nameTrack", track2.name);
-        newtrack.set("nameArtist", track2.artists[0].name);
-	newtrack.set("urlPlaylist", url);
-	newtrack.set("idUser", sessionStorage.idUser);
-        newtrack.set("votes", 0);
-        
-	
-	
+		var TrackPlaylistMusju = Parse.Object.extend("TrackPlaylistMusju");
+		var newtrack = new TrackPlaylistMusju();
 
-	newtrack.save(null,{
-						success : function(newplaylist) {
-						
-						},
-						error : function(newplaylist, error) {
-							// The save failed.
-							// error is a Parse.Error with an error code and
-							// description.
-						}
-					});
-            
-            });
- }
+		newtrack.set("urlTrack", track2.uri);
+		newtrack.set("nameTrack", track2.name);
+		newtrack.set("nameArtist", track2.artists[0].name);
+		newtrack.set("urlPlaylist", url);
+		newtrack.set("idUser", sessionStorage.idUser);
+		newtrack.set("votes", 0);
 
-function showPlaylistNoImages(uriPl){
+		newtrack.save(null, {
+			success : function(newplaylist) {
+
+			},
+			error : function(newplaylist, error) {
+				// The save failed.
+				// error is a Parse.Error with an error code and
+				// description.
+			}
+		});
+
+	});
+}
+
+function showPlaylistNoImages(uriPl) {
 	var uriPlaylist = new models.Playlist.fromURI(uriPl);
 	tempPlaylist = new models.Playlist();
 	console.log(uriPlaylist.length);
@@ -199,19 +182,20 @@ function showPlaylistNoImages(uriPl){
 	});
 	var playlistList = new views.List(tempPlaylist);
 	playlistList.node.classList.add("temporary");
-	$("#title-actualPl").html('<h3 class="blackText">'+uriPlaylist.name+'</h3>');
+	$("#title-actualPl").html(
+			'<h3 class="blackText">' + uriPlaylist.name + '</h3>');
 	$("#showActualPlaylist2").empty();
 	$("#showActualPlaylist2").append(playlistList.node);
 	window.location.href = 'spotify:app:musju:playlist';
 	$("#friend-tracks").empty();
-	
+
 	$("#friend-drop").empty();
 }
 
 function showPlaylistImages(uriPlaylist) {
 	var tempPlaylist = new models.Playlist.fromURI(uriPlaylist);
-sessionStorage.actualPlaylist=uriPlaylist;
-	
+	sessionStorage.actualPlaylist = uriPlaylist;
+
 	$("#actualSong").empty();
 	$("#nextsong").empty();
 	$("#possibleSongs").empty();
@@ -220,114 +204,127 @@ sessionStorage.actualPlaylist=uriPlaylist;
 
 		tempPlaylist2.add(models.Track.fromURI(track.uri));
 	});
-	
+
 	$("#title-actualPl").append(tempPlaylist.name);
 	// console.log(uriPlaylist.length);
 	var i = 0;
-	$.each(tempPlaylist2.tracks, function(num, track2) {
-		switch(i)
-		{
-		case 0:
-			
-			  var trackAux = models.Track.fromURI(track2.uri);
-			var playlistt = new models.Playlist();
-            playlistt.add(trackAux);
-			var playerView2 = new views.Player();
-           // playerView2.track = null; // Don't play the track right away
-            playerView2.context = playlistt;
-            $('#actualSong').append(playerView2.node);
-            $('#actualSong').append( '<a class="more medium orange awesome""  style="float:right">More</a>');
-            $('#actualSong').append( '<div id="moreInfoResults"></div>');
-            var nameS=trackAux.name;
-            var artist="";
-          
-            
-            
-        	$('a.more').click(function(e){
-        		
-        	    if($(this).parent().hasClass("open")){
-        	        $(this).parent().animate({"height":125}).removeClass("open");
-        	        $(this).html("More...");
-        	        $("#moreInfoResults").empty();
-        	        
-        	    }else{
-        	    	
-        	        $(this).parent().animate({"height":300}).addClass("open");
-        	        $(this).html("Less...");
-        	        alert(track2.album.name);
-        	        moreInfo(track2.name, track2.artists[0].name, track2.album.name);
-        	        
-        	    }
-        	    e.preventDefault();
-        	});
-  
-		  break;
-		case 1:
-			$("#nextsong").append(
-					'<a class="item" data-id="id-' + i
-							+ '" data-type="track"><img height="100" src="'
-							+ track2.image + '" /><h3 class="blackText">'
-							+ track2.name + '(' + track2.artists[0].name
-							+ ')</h3></a>');
-			
-		  break;
-		default:
-			$("#possibleSongs").append(
-					'<li class="item" data-id="id-' + i
-							+ '" data-type="track"><img height="100" src="'
-							+ track2.image + '" /><h4 class="blackText">' 
-							+ track2.name + '(' + track2.artists[0].name
-							+ ')</h4></li>');
-		}
-		
-		// $("#showActualPlaylist2").append("trackuri:"+track2.uri+"cover"+track2.image+"artist"+track2.artists[0].name);
-		i++;
-		
+	$
+			.each(
+					tempPlaylist2.tracks,
+					function(num, track2) {
+						switch (i) {
+						case 0:
 
-	});
+							var trackAux = models.Track.fromURI(track2.uri);
+							var playlistt = new models.Playlist();
+							playlistt.add(trackAux);
+							var playerView2 = new views.Player();
+							// playerView2.track = null; // Don't play the track right away
+							playerView2.context = playlistt;
+							$('#actualSong').append(playerView2.node);
+							$('#actualSong')
+									.append(
+											'<a class="more medium orange awesome""  style="float:right">More</a>');
+							$('#actualSong').append(
+									'<div id="moreInfoResults"></div>');
+							var nameS = trackAux.name;
+							var artist = "";
 
-	
+							$('a.more').click(
+									function(e) {
+
+										if ($(this).parent().hasClass("open")) {
+											$(this).parent().animate({
+												"height" : 125
+											}).removeClass("open");
+											$(this).html("More...");
+											$("#moreInfoResults").empty();
+
+										} else {
+
+											$(this).parent().animate({
+												"height" : 300
+											}).addClass("open");
+											$(this).html("Less...");
+											alert(track2.album.name);
+											moreInfo(track2.name,
+													track2.artists[0].name,
+													track2.album.name);
+
+										}
+										e.preventDefault();
+									});
+
+							break;
+						case 1:
+							$("#nextsong")
+									.append(
+											'<a class="item" data-id="id-'
+													+ i
+													+ '" data-type="track"><img height="100" src="'
+													+ track2.image
+													+ '" /><h3 class="blackText">'
+													+ track2.name + '('
+													+ track2.artists[0].name
+													+ ')</h3></a>');
+
+							break;
+						default:
+							$("#possibleSongs")
+									.append(
+											'<li class="item" data-id="id-'
+													+ i
+													+ '" data-type="track"><img height="100" src="'
+													+ track2.image
+													+ '" /><h4 class="blackText">'
+													+ track2.name + '('
+													+ track2.artists[0].name
+													+ ')</h4></li>');
+						}
+
+						// $("#showActualPlaylist2").append("trackuri:"+track2.uri+"cover"+track2.image+"artist"+track2.artists[0].name);
+						i++;
+
+					});
+
 	window.location.href = 'spotify:app:musju:playlist';
 	$("#friend-tracks").empty();
-	
+
 	$("#friend-drop").empty();
 	$("#friend-drop").append("Drag and drop your playlist!");
-	
+
 	// setInterval(function() {
-		// updatePl()
+	// updatePl()
 	// }, 5000);
 
 }
 
 function updatePl() {
 
-	
-	
-	
 }
-
 
 function getPlaylistUser() {
 
-	
 	var PlaylistMusju = Parse.Object.extend("PlaylistMusju");
 	var query = new Parse.Query(PlaylistMusju);
-	
+
 	query.equalTo("idUser", sessionStorage.idUser);
 	query.find({
 		success : function(results) {
-			var i=0;
-			
-		while(i<results.length){
-			
-				url="'"+results[i].get("url")+"'";
-				
-				$("#playlistsUser").append('<a href="spotify:app:musju:playlist" onclick="showPlaylistImages('+url+')" class="large orange awesome">'+results[i].get("name")+' &raquo;</a> <br /><br />');
-			i++;
-				}
-		
-		
-		
+			var i = 0;
+
+			while (i < results.length) {
+
+				url = "'" + results[i].get("url") + "'";
+
+				$("#playlistsUser").append(
+						'<a href="spotify:app:musju:playlist" onclick="showPlaylistImages('
+								+ url + ')" class="large orange awesome">'
+								+ results[i].get("name")
+								+ ' &raquo;</a> <br /><br />');
+				i++;
+			}
+
 		},
 		error : function(error) {
 			alert("Error: " + error.code + " " + error.message);
@@ -335,57 +332,72 @@ function getPlaylistUser() {
 	});
 }
 
-function requestSong(){
-	  var track = models.Track.fromURI(sessionStorage.requestSongUri);
-      // Create a playlist object from a URI
-     
-      var tempPlaylist = new models.Playlist.fromURI(sessionStorage.actualPlaylist);
-      // Add the track
-      tempPlaylist.add(track);
-      var last=tempPlaylist.length;
-      last--;
-      $("#possibleSongs").append(
-				'<li class="item" ><img height="100" src="'
-						+ tempPlaylist.tracks[last].image + '" /><h4 class="blackText">' 
-						+ tempPlaylist.tracks[last].name + '(' + tempPlaylist.tracks[last].artists[0].name
-						+ ')</h4></li>');
-      
-      
-      var TrackPlaylistMusju = Parse.Object.extend("TrackPlaylistMusju");
-  	var newtrack = new TrackPlaylistMusju();
-  	
-  	
-  	newtrack.set("urlTrack", track.uri);
-  	newtrack.set("nameTrack", track.name);
-          newtrack.set("nameArtist", track.artists[0].name);
-  	newtrack.set("urlPlaylist", sessionStorage.actualPlaylist);
-  	newtrack.set("idUser", sessionStorage.idUser);
-          newtrack.set("votes", 1);
-          
-  	
-  	
-
-  	newtrack.save(null,{
-  						success : function(newplaylist) {
-  						
-  						},
-  						error : function(newplaylist, error) {
-  							// The save failed.
-  							// error is a Parse.Error with an error code and
-  							// description.
-  						}
-  					});
-              
-              }
-
-	
+function requestSong() {
+	var track = models.Track.fromURI(sessionStorage.requestSongUri);
+	// Create a playlist object from a URI
 
 
-function moreInfo(name, artist,album){
-	
-	 /*
-		 * $.get("http://ws.spotify.com/search/1/track.json", { q: ""+name+"
-		 * "+artist }, function(data) { alert("data"+data); });
-		 */
-	searchMoreInfo(name,artist,album);
+
+	var TrackPlaylistMusju = Parse.Object.extend("TrackPlaylistMusju");
+	var query = new Parse.Query(TrackPlaylistMusju);
+
+	query.equalTo("urlTrack", track.uri);
+	query.first({
+		success : function(results) {
+
+			alert("This track is already in the playlist");
+
+		},
+		error : function(error) {
+			
+			var tempPlaylist = new models.Playlist.fromURI(
+					sessionStorage.actualPlaylist);
+			// Add the track
+			tempPlaylist.add(track);
+			var last = tempPlaylist.length;
+			last--;
+			$("#possibleSongs")
+					.append(
+							'<li class="item" ><img height="100" src="'
+									+ tempPlaylist.tracks[last].image
+									+ '" /><h4 class="blackText">'
+									+ tempPlaylist.tracks[last].name + '('
+									+ tempPlaylist.tracks[last].artists[0].name
+									+ ')</h4></li>');
+
+			var TrackPlaylistMusju = Parse.Object.extend("TrackPlaylistMusju");
+			var newtrack = new TrackPlaylistMusju();
+
+			newtrack.set("urlTrack", track.uri);
+			newtrack.set("nameTrack", track.name);
+			newtrack.set("nameArtist", track.artists[0].name);
+			newtrack.set("urlPlaylist", sessionStorage.actualPlaylist);
+			newtrack.set("idUser", sessionStorage.idUser);
+			newtrack.set("votes", 1);
+
+			newtrack.save(null, {
+				success : function(newplaylist) {
+
+				},
+				error : function(newplaylist, error) {
+					// The save failed.
+					// error is a Parse.Error with an error code and
+					// description.
+				}
+			});
+			
+
+		}
+
+	});
+
+}
+
+function moreInfo(name, artist, album) {
+
+	/*
+	 * $.get("http://ws.spotify.com/search/1/track.json", { q: ""+name+"
+	 * "+artist }, function(data) { alert("data"+data); });
+	 */
+	searchMoreInfo(name, artist, album);
 }
